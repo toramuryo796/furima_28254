9a5335167a15ab1eb34fe52fbdb08c6a74295572
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-11 00:30:51 +0900

    devise_parameter_sanitizerメソッド記述

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :family_name, :first_name_reading, :family_name_reading, :birth_date])    
+  end
+
 end
