950190b6910c65865cdf9e90c672e18952603e63
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-11 12:16:10 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
     validates :email, uniqueness: true
     validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is invalid, Include '@'."}
     validates :password, length: { minimum: 6 }
+    validates :password, format: {/\A[a-zA-Z0-9]+\Z/, message: "is invalid. Input half_width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/, message: "is invalid. Input full_width characters."}
     validates :family_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/, message: "is invalid. Input full_width characters."}
     validates :first_name_reading, format: {with: /\A[ァ-ヶー-]+\z/, message: "is invalid. Input half_width characters."}
