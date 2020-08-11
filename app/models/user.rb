4b4c37420eb747125c7b2e6f3893272ea8353891
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-11 18:10:45 +0900

    バリデーション 修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,8 @@ class User < ApplicationRecord
         
   validates :birth_date, presence: true
   with_options presence: true do
-    validates :nickname, length: {maximum: 40}
-    validates :email, uniqueness: true
-    validates :email, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is invalid, Include '@'."}
-    validates :password, length: { minimum: 6 }
+    validates :nickname, length: {maximum: 40}, format: {with: /\A[a-zA-Zぁ-んァ-ンー-龥]+\Z/}
+    validates :email, uniqueness: true, format: {with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i, message: "is invalid, Include '@'."}
     validates :password, format: {with: /\A[a-zA-Z0-9]+\Z/, message: "is invalid.Input half_width characters"}
     validates :first_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/, message: "is invalid. Input full_width characters."}
     validates :family_name, format: {with: /\A[ぁ-んァ-ンー-龥]+\z/, message: "is invalid. Input full_width characters."}
