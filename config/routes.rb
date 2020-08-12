6faeb0a7aaa9e76c46efd327df475135308ba96b
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-12 19:33:30 +0900

    items 購入画面 コントローラー記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
-  resources :items, only: :index
+  resources :items, only: [:index, :show, :new, :create] do
+    resources :purchases, only: [:create]
+  end
 end
