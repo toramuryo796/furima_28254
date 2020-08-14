6d8a1cbd11948db8c3460899ffa167cc54f96c36
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-14 16:06:02 +0900

    商品出品 モデルの単体テスト　FactotyBot生成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :item do
-    
+    title           {Faker::Lorem.word}
+    explain         {Faker::Lorem.paragraph}
+    price           {Faker::Number.within(range: 300..9999999)}
+    category_id     {Faker::Number.within(1..10)}
+    status_id       {Faker::Number.within(1..6)}
+    fee_id          {Faker::Number.within(1..2)}
+    origin_area_id  {Faker::Number.within(1..47)}
+    take_days_id    {Faker::Number.within(1..3)}
   end
 end
