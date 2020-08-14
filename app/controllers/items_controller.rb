4fa52a6e4f7778fbfff83ee5092992b7b09965db
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-14 16:09:32 +0900

    商品出品 販売手数料 実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,14 +10,26 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-
+  
   def create
     @item = Item.new(item_params)
+    price = @item.price
+    if price 
+      @commission = (price * 0.1).to_i
+    end
+    if price
+      @profit = price - @commission
+    end
     if @item.save
       redirect_to root_path
     else
       render :new
     end
+  end
+  
+  def commission
+    item = Item.new(item_params)
+    render json:{ item: item }
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root "items#index"
+  post 'items/commission', to: 'items#commission'
   resources :items, only: [:index, :show, :new, :create] do
     resources :purchases, only: [:create]
   end
+  
 end
