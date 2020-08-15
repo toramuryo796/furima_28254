ea9b1769e82fd046f7c2e4a38e1a35e0610498fe
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-16 08:18:35 +0900

    バリデーション 修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,11 +10,17 @@ class Item < ApplicationRecord
   belongs_to_active_hash :origin_area
   belongs_to_active_hash :take_day
 
-  validates :category_id, :status_id, :fee_id, :origin_area_id, :take_days_id, presence: true
-
   with_options presence: true do
     validates :title, length: {maximum: 40, message: "is invalid. Input within 40 characters."}
     validates :explain, length: {maximum: 1000, message: "is invalid. Input within 40 characters."}
-    validates :price, numericality: {only_integer:true, greater_than:300, less_than:9999999, message: "is invalid. Input number from 300 to 9,999,999."}, format: {with: /\A[0-9]+\Z/, message: "is invalid. Input number."} 
+    validates :price, numericality: {only_integer:true, greater_than:300, less_than:10000000, message: "is invalid. Input number from 300 to 9,999,999."}, format: {with: /\A[0-9]+\Z/, message: "is invalid. Input number."} 
+    validates :category_id, numericality: {only_integer: true, greater_than:1, less_than:11}
+    validates :status_id, numericality: {only_integer: true, greater_than:1, less_than:7}
+    validates :fee_id, numericality: {only_integer: true, greater_than:1, less_than:3}
+    validates :origin_area_id, numericality: {only_integer: true, greater_than:1, less_than:48}
+    validates :take_days_id, numericality: {only_integer: true, greater_than:1, less_than:4}
   end
+
+  validates :image, presence: true
+    
 end
