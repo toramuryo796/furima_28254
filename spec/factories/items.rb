fbc0721887e27659a031dfc3096e8f0addf3d574
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-16 08:18:09 +0900

    FactoryBot 修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,12 +1,13 @@
 FactoryBot.define do
   factory :item do
-    title           {Faker::Lorem.characters(number: 10)}
-    explain         {Faker::Lorem.characters(number: 300)}
-    price           {Faker::Number.within(range: 300..9999999)}
-    category_id     {Faker::Number.within(range: 1..10)}
-    status_id       {Faker::Number.within(range:1..6)}
-    fee_id          {Faker::Number.within(range:1..2)}
-    origin_area_id  {Faker::Number.within(range:1..47)}
-    take_days_id    {Faker::Number.within(range:1..3)}
+    title           {"あいうえお絵否定受けだふぇふイウレスskdjfkfher"}
+    explain         {"かいくらああすへ津田でソニンのけヘアあづ雨おせasdjjfisafnjakshfmjns12443"}
+    price           {"92357"}
+    category_id     {8}
+    status_id       {5}
+    fee_id          {2}
+    origin_area_id  {42}
+    take_days_id    {3}
+    association     :user
   end
 end
