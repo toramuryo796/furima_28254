53d8c2a72de71b24c1283825dfd26bbe1a6840fc
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-16 16:23:06 +0900

    発送日数カラム名の変更

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :purchase
   has_one_attached :image
-
+  
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
   belongs_to_active_hash :status
@@ -14,11 +14,11 @@ class Item < ApplicationRecord
     validates :title, length: {maximum: 40, message: "is invalid. Input within 40 characters."}
     validates :explain, length: {maximum: 1000, message: "is invalid. Input within 40 characters."}
     validates :price, numericality: {only_integer:true, greater_than:299, less_than:10000000, message: "is invalid. Input number from 300 to 9,999,999."}, format: {with: /\A[0-9]+\Z/, message: "is invalid. Input number."} 
-    validates :category_id, numericality: {only_integer: true, greater_than:1, less_than:11}
-    validates :status_id, numericality: {only_integer: true, greater_than:1, less_than:7}
-    validates :fee_id, numericality: {only_integer: true, greater_than:1, less_than:3}
-    validates :origin_area_id, numericality: {only_integer: true, greater_than:1, less_than:48}
-    validates :take_days_id, numericality: {only_integer: true, greater_than:1, less_than:4}
+    validates :category_id, numericality: {only_integer: true, greater_than:0, less_than:11}
+    validates :status_id, numericality: {only_integer: true, greater_than:0, less_than:7}
+    validates :fee_id, numericality: {only_integer: true, greater_than:0, less_than:3}
+    validates :origin_area_id, numericality: {only_integer: true, greater_than:0, less_than:48}
+    validates :take_day_id, numericality: {only_integer: true, greater_than:0, less_than:4}
   end
 
   validates :image, presence: true
