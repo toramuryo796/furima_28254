70bdf3fa1d5c6840e84f5b83e23d6f5619f99a0f
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-17 20:25:06 +0900

    コントローラー 実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,6 +44,12 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
+
   def commission
   end
 
