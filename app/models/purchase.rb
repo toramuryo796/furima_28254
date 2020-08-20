3ae2c788b503c29660bee6a8ac4595faf6c5b9ff
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-20 15:24:04 +0900

    バリデーション設定

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -7,11 +7,10 @@ class Purchase < ApplicationRecord
 
   with_options presence: true do
     validates :price
-    validates :postal_code
-    validates :origin_area_id
-    validates :city
-    validates :house_number
-    validates :building_name
-    validates :phone_number
+    validates :postal_code,    format:{with: /\A\d{3}[-]\d{4}+\Z/ , message: "is invalid. Input number with '-'."}
+    validates :origin_area_id, numericality: {only_integer: true, greater_than:0, less_than:48}
+    validates :city,           format: {with: /\A[ぁ-んァ-ンー-龥]+\Z/, message: "is invalid. Input full_width characters." }
+    validates :house_number,   format: {with: /\A[0-9ぁ-んァ-ンー-龥]+\Z/, message: "is invalid. Input full_width characters or number."} 
+    validates :phone_number,   numericality: {only_integer: true, message: "is invalid. Input number."}
   end
 end
