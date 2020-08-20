c786e6f6e51dc7034ecfa8a510dff8aa37978b3a
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-20 15:23:13 +0900

    未ログイン→ログイン画面, 出品者→トップページ

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,13 +1,15 @@
 class PurchasesController < ApplicationController
+  before_action :find_item, only: [:index, :create, :pay_item, :move_to_root]
+  before_action :move_to_login, only: [:index, :create]
+  before_action :move_to_root, only: [:index, :create]
+
   def index
-    @item = Item.find_by(params[:id])
+    # binding.pry
     @purchase = Purchase.new
   end
 
   def create
-    @item = Item.find_by(params[:id])
     price = @item.price
-    # binding.pry
     @purchase = Purchase.new(price: price, postal_code: purchase_params[:postal_code], origin_area_id: purchase_params[:origin_area_id], city: purchase_params[:city], house_number: purchase_params[:house_number], building_name: purchase_params[:building_name], phone_number: purchase_params[:phone_number], user_id: purchase_params[:user_id], item_id: purchase_params[:item_id])
     if @purchase.valid?
       pay_item
@@ -26,13 +28,30 @@ class PurchasesController < ApplicationController
     params.permit(:token, :postal_code, :origin_area_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id,  item_id: params[:item_id])
   end
 
+  def find_item
+    @item = Item.find_by(id: purchase_params[:item_id]) 
+  end
+
   def pay_item
-    @item = Item.find_by(params[:id])
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_params[:token],
       currency:'jpy'
     )
+  end
+
+  def move_to_login
+    # binding.pry
+    unless user_signed_in?   
+      redirect_to new_user_session_path
+    end
+  end
+
+  def move_to_root
+    # binding.pry
+    if current_user == @item.user
+      redirect_to root_path
+    end
   end
 end
