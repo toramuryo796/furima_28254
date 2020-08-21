1cfd8fabf5b3a0e1205e4519d6415a2550528a8a
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-21 16:58:02 +0900

    message修正

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -4,18 +4,18 @@ class Purchase < ApplicationRecord
   
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :origin_area
-  postal_code = {with: /\A\d{3}[-]\d{4}+\Z/ , message: "is invalid. Input number with '-'."}
+  postal_code =  /\A\d{3}[-]\d{4}+\Z/ 
   origin_area = {only_integer: true, greater_than:0, less_than:48}
-  full_characters = {with: /\A[ぁ-んァ-ンー-龥]+\Z/, message: "is invalid. Input full_width characters."}
-  number_full_characters = {with: /\A[0-9ぁ-んァ-ンー-龥-]+\Z/, message: "is invalid. Input full_width characters or number."} 
-  phone = {with: /\A0[0-9]{9,10}\Z/, message: "is invalid. Input 10 or 11 numbers"}
+  full_characters =  /\A[ぁ-んァ-ンー-龥]+\Z/
+  number_full_characters = /\A[0-9ぁ-んァ-ンー-龥-]+\Z/ 
+  phone = /\A0[0-9]{9,10}\Z/
 
   with_options presence: true do
     validates :price
-    validates :postal_code,    format: postal_code
+    validates :postal_code,    format: {with: postal_code, message: "is invalid. Input number with '-'."}
     validates :origin_area_id, numericality: origin_area
-    validates :city,           format: full_characters
-    validates :house_number,   format: number_full_characters
-    validates :phone_number,   format: phone
+    validates :city,           format: {with: full_characters, message: "is invalid. Input full_width characters."}
+    validates :house_number,   format: {with: number_full_characters, message: "is invalid. Input full_width characters or number."}
+    validates :phone_number,   format: {with: phone, message: "is invalid. Input 10 or 11 numbers"}
   end
 end
