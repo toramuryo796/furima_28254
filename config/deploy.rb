be0156cfbb5809f8c6e61eb37814c7fd346706ea
toramuryo796 <68458635+toramuryo796@users.noreply.github.com>
2020-08-25 19:41:44 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima_28254'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:toramuryo796/furima28254.git'
+set :repo_url,  'git@github.com:toramuryo796/furima_28254.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
